c3070ec4a5acc28a70c48f79b37c1314099f3cb8
Parimi Satwika <parimisatwika2519@gmail.com>
2025-08-12 08:53:06 +0530

    Add files via upload

---
+++ Logic Building/An Approach To Logic Building/altnums.java
@@ -0,0 +1,8 @@
+class altnums{
+    public static void main(String[] args){
+        for(int i=0;i<=100;i++){
+            System.out.print(i+" ");
+            i++;
+        }
+    }
+}
---
+++ Logic Building/An Approach To Logic Building/rarj.java
@@ -0,0 +1,15 @@
+class rarj { // right angled - right justified stars
+    public static void main(String[] args) {
+        int rows = 10;
+        for (int i = 1; i <= rows; i++) {
+            for (int space = 1; space <= rows - i; space++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
