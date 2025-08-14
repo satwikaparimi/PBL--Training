747c6b997f596fe823133a85c153f08d50c5f050
Parimi Satwika <parimisatwika2519@gmail.com>
2025-08-14 09:49:55 +0530

    Add files via upload

---
+++ Logic Building/Number Based Problems/threevarope.java
@@ -0,0 +1,17 @@
+class threevarope{//three variable operation
+    public static void main(String[] args){
+        int a=10;
+        int b=15;
+        int c=20;
+        int t1,t2,t3;
+        t1=a;
+        t2=b;
+        t3=c;
+        b=t1;
+        c=t2;
+        a=t3;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
