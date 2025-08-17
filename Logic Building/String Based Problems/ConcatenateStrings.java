5be758850c3d94494722db8b5bfd0fd2ebb48810
Parimi Satwika <parimisatwika2519@gmail.com>
2025-08-17 17:35:00 +0530

    Add files via upload

---
+++ Logic Building/String Based Problems/ConcatenateStrings.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class ConcatenateStrings {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first string: ");
+        String s1 = sc.nextLine();
+        System.out.print("Enter second string: ");
+        String s2 = sc.nextLine();
+        String s3 = s1 + s2;
+        System.out.println("Concatenated string: " + s3);
+    }
+}
